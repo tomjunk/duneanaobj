58019a000e8d421bfebf515917977b5209b5746b
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2021-11-15 13:45:59 -0600

    tracks now have Evis field too

--- duneanaobj/StandardRecord/SRTrack.cxx
+++ duneanaobj/StandardRecord/SRTrack.cxx
@@ -9,6 +9,6 @@
 
 std::ostream &operator<<(std::ostream &stream, const caf::SRTrack &tr)
 {
-  stream << "caf::SRTrack with start=" << tr.start << "  end=" << tr.end << "  end_dir=" << tr.end_dir;
+  stream << "caf::SRTrack with start=" << tr.start << "  end=" << tr.end << "  end_dir=" << tr.end_dir << "  visE=" << tr.Evis << std::endl;
   return stream;
 }
