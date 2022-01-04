282ea6d8625c601f02c3057dacd3fd6d753c73eb
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2022-01-04 12:02:49 -0500

    add a track-start unit vector as well

--- duneanaobj/StandardRecord/SRTrack.cxx
+++ duneanaobj/StandardRecord/SRTrack.cxx
@@ -9,6 +9,6 @@
 
 std::ostream &operator<<(std::ostream &stream, const caf::SRTrack &tr)
 {
-  stream << "caf::SRTrack with start=" << tr.start << "  end=" << tr.end << "  end_dir=" << tr.end_dir << "  visE=" << tr.Evis << std::endl;
+  stream << "caf::SRTrack with start=" << tr.start << "  end=" << tr.end << "   start dir=" << tr.dir << "  end_dir=" << tr.end_dir << "  visE=" << tr.Evis << std::endl;
   return stream;
 }
--- duneanaobj/StandardRecord/SRTrack.h
+++ duneanaobj/StandardRecord/SRTrack.h
@@ -17,7 +17,8 @@ namespace caf
     public:
       SRVector3D start;      ///< Track 3D start point
       SRVector3D end;        ///< Track 3D end point
-      SRVector3D enddir;    ///< Unit vector representing estimate of track direction *taken from endpoint*
+      SRVector3D dir;        ///< Unit vector representing estimate of track direction *taken from start point*
+      SRVector3D enddir;     ///< Unit vector representing estimate of track direction *taken from endpoint*
 
       float Evis  = -999.;   ///< Visible energy in voxels corresponding to this track
 
