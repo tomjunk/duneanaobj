4bc6c2ae61449f35a6355391bbf7d391667beb9f
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2023-03-13 14:46:52 -0400

    remove all the top-level reco branches that are now in SRCommonRecoBranch

--- duneanaobj/StandardRecord/StandardRecord.h
+++ duneanaobj/StandardRecord/StandardRecord.h
@@ -17,6 +17,7 @@
 
 #include "duneanaobj/StandardRecord/SRBeamBranch.h"
 #include "duneanaobj/StandardRecord/SRMeta.h"
+#include "duneanaobj/StandardRecord/SRCommonRecoBranch.h"
 #include "duneanaobj/StandardRecord/SRNDBranch.h"
 #include "duneanaobj/StandardRecord/SRTruthBranch.h"
 
@@ -46,91 +47,12 @@ namespace caf
 
     SRTruthBranch mc;
 
-    // Reco info
-    float eRec_FromDep; // Unified parameterized reco that can be used at near and far. Should only be used for missing proton energy fake data studies that cannot use the CVN FD Reco
-    float Ev_reco; // for ND?
-    float Ev_reco_nue;
-    float Ev_reco_numu;
-    float mvaresult;
-    float mvanue;
-    float mvanumu;
-    float cvnnue;
-    float cvnnumu;
-    float cvnnutau;
-    float cvnnc;
-    int reco_q;
-    float Elep_reco;
-    float theta_reco;
-    int reco_lepton_pdg;
+    SRCommonRecoBranch common;
 
-    float RecoLepEnNue;
-    float RecoHadEnNue;
-    float RecoLepEnNumu;
-    float RecoHadEnNumu;
 
-    double pileup_energy;
 
     SRNDBranch nd;
 
-    int RecoMethodNue;  // 1 = longest reco track + hadronic, 2 = reco shower with highest charge + hadronic, 3 = all hit charges, -1 = not set
-    int RecoMethodNumu; // 1 = longest reco track + hadronic, 2 = reco shower with highest charge + hadronic, 3 = all hit charges, -1 = not set
-    int TrackMomMethodNumu;
-
-    // ND pseudo-reconstruction flags
-    int reco_numu;
-    int reco_nue;
-    int reco_nc;
-
-    // CW: added for the ND cuts Chris (M) wants
-    // ND containment flags
-    int muon_contained;
-    int muon_tracker;
-    int muon_ecal;
-    int muon_exit;
-    float Ehad_veto;
-
-    // To prevent errors when compiling DUNENDSysts
-    float nue_pid;
-    float numu_pid;
-
-    // Containment flag
-    int LongestTrackContNumu;
-
-
-    float vtx_x;
-    float vtx_y;
-    float vtx_z;
-
-
-    // Reconstructed energy of particles by species
-    float eRecoP;
-    float eRecoN;
-    float eRecoPip;
-    float eRecoPim;
-    float eRecoPi0;
-    float eRecoOther;
-
-    //At FD
-    float eDepP;
-    float eDepN;
-    float eDepPip;
-    float eDepPim;
-    float eDepPi0;
-    float eDepOther;
-
-    // ND? -- eliminate, should get from reco tracks
-    SRVector3D LepEndpoint;
-
-
-    // CVN outputs
-    float CVNResultIsAntineutrino;
-    float CVNResultNue, CVNResultNumu, CVNResultNutau, CVNResultNC; // flavour
-    float CVNResult0Protons, CVNResult1Protons, CVNResult2Protons, CVNResultNProtons; // #protons
-    float CVNResult0Pions, CVNResult1Pions, CVNResult2Pions, CVNResultNPions; // #pions
-    float CVNResult0Pizeros, CVNResult1Pizeros, CVNResult2Pizeros, CVNResultNPizeros; // #pizeros
-    float CVNResult0Neutrons, CVNResult1Neutrons, CVNResult2Neutrons, CVNResultNNeutrons; // #neutrons
-
-    float RegCNNNueE;
 
     // sigmas
     float sigma_Ev_reco;
