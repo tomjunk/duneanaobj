59c76ba8d1f0d2a7c5ffb7d7ee4818e30f265a2f
Jeremy Wolcott <jeremy.wolcott@tufts.edu>
2023-04-24 15:02:52 -0400

    rewrite Assns to identify interaction using detector reco obj IDs

--- duneanaobj/StandardRecord/SRNDTrackAssn.h
+++ duneanaobj/StandardRecord/SRNDTrackAssn.h
@@ -4,17 +4,25 @@
 #ifndef DUNEANAOBJ_SRNDTRACKASSN_H
 #define DUNEANAOBJ_SRNDTRACKASSN_H
 
+#include "duneanaobj/StandardRecord/SRNDLAr.h"
+#include "duneanaobj/StandardRecord/SRTMS.h"
+#include "duneanaobj/StandardRecord/SRMINERvA.h"
+#include "duneanaobj/StandardRecord/SRGAr.h"
 
 namespace caf
 {
   class SRNDTrackAssn
   {
     public:
-      int larid         = -1;        ///< index of ND-LAr track
-      int tmsid         = -1;        ///< index of TMS track
-      int minervaid     = -1;        ///< index of MINERvA track
-      float transdispl  = -999.;     ///< perpendicular distance between the two tracks at longitudinal position of matching point
-      float angdispl    = -999.;     ///< angular difference between the two tracks at longitudinal position of matching point
+      SRNDLAr::ID   larid;     ///< ND-LAr track identifier.  Get the actual SRTrack object using SRNDLAr::Reco<Track>() with this ID, e.g.`sr.nd.lar.Reco<Track>(sr.nd.trkmatch.extrap[1].larid)`
+      SRTMS::ID     tmsid;     ///< TMS track identifier.   Get the actual SRTrack object using SRTMS::Track() with this ID, e.g.`sr.nd.lar.Track(sr.nd.trkmatch.extrap[1].tmsid)`
+      SRMINERvA::ID minervaid; ///< MINERvA track identifier.
+      SRGAr::ID     garid;     ///< GAr track identifier.
+
+
+      static constexpr float NaN = std::numeric_limits<float>::signaling_NaN();
+      float transdispl  = NaN;     ///< perpendicular distance between the two tracks at longitudinal position of matching point
+      float angdispl    = NaN;     ///< angular difference between the two tracks at longitudinal position of matching point
 
       SRTrack trk;                   ///< new track object generated from synthesis of matched parts
   };
