b212485288fb4feaa7d7dbfe42c405cdb89095c1
Thomas Junk <trj@fnal.gov>
2023-08-14 13:42:07 -0500

    remove hack now that castxml is at v0_5_1

--- duneanaobj/StandardRecord/SRTrueInteraction.h
+++ duneanaobj/StandardRecord/SRTrueInteraction.h
@@ -21,9 +21,13 @@
 // namespace std{class string{};}
 // #endif
 
-#ifdef __GNUC__
-#undef _GLIBCXX_HAVE_BUILTIN_IS_CONSTANT_EVALUATED
-#endif
+// a less ugly hack, works with castxml v0_4_2 and e20, c7 and e26 but not c14
+// not needed for castxml v0_5_1
+
+//#ifdef __GNUC__
+//#undef _GLIBCXX_HAVE_BUILTIN_IS_CONSTANT_EVALUATED
+//#endif
+
 #include <string>
 
 #include <vector>
